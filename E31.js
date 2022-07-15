f9f7aed64a5c9afd08f692c1d8a6e060207c19c8
mariolanzaldo <100377731+mariolanzaldo@users.noreply.github.com>
2022-07-15 14:54:15 -0500

    Update E31.js

--- E31.js
+++ E31.js
@@ -3,7 +3,6 @@ const container = document.querySelector('.container');
 container.addEventListener('click', event => {
     console.log(event.target.textContent.includes('Toggle'))
     if (event.target.textContent.includes('Toggle') && event.target.nextElementSibling.style.display === 'flex') {
-        console.log('a')
         event.target.nextElementSibling.style.display = 'none';
     } else {
         event.target.nextElementSibling.style.display = 'flex';
