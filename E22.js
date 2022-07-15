2ee0775b94a67da1ca1e51636cffa66b0de39315
mariolanzaldo <100377731+mariolanzaldo@users.noreply.github.com>
2022-07-15 14:46:59 -0500

    Update E22.js

--- E22.js
+++ E22.js
@@ -1,15 +1,15 @@
-function random(number){
+function random(number) {
     return Math.floor(Math.random() * number);
 }
 
-function randColor(){
+function randColor() {
     return `rgb(${random(255)}, ${random(255)}, ${random(255)})`;
 }
 
 const button = document.querySelector('.button');
 const div = document.querySelector('.colored');
 
-button.addEventListener('click',event =>{
+button.addEventListener('click', event => {
     console.log('clicked');
     div.style.background = randColor();
 });
