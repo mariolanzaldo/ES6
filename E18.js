6e2e155efff026d2d23b12db0e0f36dd0d8e1498
mariolanzaldo <100377731+mariolanzaldo@users.noreply.github.com>
2022-08-11 16:44:53 -0500

    Update E18.js

--- E18.js
+++ E18.js
@@ -1,4 +1,5 @@
 async function loadData(url) {
+    const IDField = document.querySelector('.ID');
     const firstNameInput = document.querySelector('.form_control.firstName');
     const lastNameInput = document.querySelector('.form_control.lastName');
     const phoneInput = document.querySelector('.form_control.phone');
@@ -13,24 +14,27 @@ async function loadData(url) {
 
     const data = await response.json();
     for (const key in data) {
-        const { name: { firstName, lastName, phone } } = data[key];
+        const { id, name: { firstName, lastName }, phone } = data[key];
+        IDField.textContent = id;
         firstNameInput.value = `${firstName}`;
         lastNameInput.value = `${lastName}`;
         phoneInput.value = `${phone}`;
     }
 }
 
-async function saveTr(self) {
+async function saveTr(self, ID) {
     const [firstNameInput, lastNameInput, phoneInput] = self.closest('tr').querySelectorAll('input');
 
     if (firstNameInput.value && lastNameInput.value && phoneInput.value) {
         const currentCandidate = {
+            id: ID,
             name: {
                 firstName: firstNameInput.value,
                 lastName: lastNameInput.value,
-                phone: phoneInput.value
-            }
+            },
+            phone: phoneInput.value
         }
+
         const response = await fetch('./E18.json', {
             method: 'PUT',
             body: JSON.stringify(currentCandidate),
@@ -43,7 +47,7 @@ async function saveTr(self) {
 
         const data = await response.json();
 
-        loadData('./E18.json');
+        await loadData('./E18.json');
     } else if (!firstNameInput.value && !lastNameInput.value && !phoneInput.value) {
         alert('There is nothing to be saved!');
     }
@@ -57,19 +61,19 @@ async function createTr(url) {
     tableBody.appendChild(trClone);
 
     const newCandidate = {
+        id: Date.now(),
         name: {
             firstName: undefined,
             lastName: undefined,
-            phone: undefined
-        }
+        },
+        phone: undefined
     };
 
     const response = await fetch(url, {
         method: 'POST',
         body: JSON.stringify(newCandidate),
-        headers: new Headers()
+        headers: new Headers(),
     });
-
     if (!response.ok) {
         console.error(`${{ status: response.status, statusText: response.statusText }}`);
     }
@@ -89,14 +93,15 @@ function cleanFirstTr(firstTr) {
 }
 
 
-async function removeTr(self) {
+async function removeTr(self, ID) {
     if (self.closest('tbody').childElementCount == 1) {
         alert('You are not allowed to delete this!');
     } else {
         const element = self.closest('tr');
         await fetch('./E18.json', {
             method: 'DELETE',
-            headers: new Headers()
+            headers: new Headers(),
+            body: `${ID}`
         });
 
         if (!response.ok) {
@@ -113,9 +118,11 @@ table.addEventListener('click', function (event) {
     if (event.target.className === 'success') {
         createTr("./E18.json");
     } else if (event.target.className === 'remove') {
-        removeTr(event.target);
+        const ID = document.querySelector('.ID').textContent;
+        removeTr(event.target, ID);
     } else if (event.target.className === 'save') {
-        saveTr(event.target);
+        const ID = document.querySelector('.ID').textContent;
+        saveTr(event.target, ID);
     }
 });
 
