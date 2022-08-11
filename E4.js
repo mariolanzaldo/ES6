f2d80d595abd2e5b3c210b24c2f26e87609973e9
mariolanzaldo <100377731+mariolanzaldo@users.noreply.github.com>
2022-08-10 19:32:50 -0500

    Update E4.js

--- E4.js
+++ E4.js
@@ -1,14 +1,16 @@
-function* flat(input, ...leng) {
-    if (Array.isArray(input)) {
-        yield* flat(...input);
-    } else {
-        yield input;
+function flat (input) {
+    let  result = [];
+    for (let i = 0, length = input.length; i < length; i++) {
+      const value = input[i];
+      if (Array.isArray(value)) {
+        flat(value);
+      } else {
+        result.push(value);
+      }
     }
-    if (leng.length) {
-        yield* flat(...leng);
-    }
-}
+    return result;
+  };
 
 const arr = [1, 2, 3, [4, 5, [6, [7, [8]]]], 8, 9];
-const output = [...flat(arr)];
+const output = flat(arr);
 console.log(output);
