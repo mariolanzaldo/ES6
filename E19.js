57c22bf17de60326fdc06098fd6902ef414a46d9
mariolanzaldo <100377731+mariolanzaldo@users.noreply.github.com>
2022-08-11 19:44:27 -0500

    Update E19.js

--- E19.js
+++ E19.js
@@ -1,51 +1,45 @@
-function check(str, pattern, output) {
-    let chars = '';
-    for (let i = 32; i <= 126; i++) {
-        if (i !== 42) {
-            chars += String.fromCharCode(i);
-        }
-    }
-    if (pattern.includes('*')) {
-        for (const element of chars) {
-            let newPattern = pattern.replace('*', element);
-            check(str, newPattern, output);
-        }
-    }
+function check(str, pattern) {
+    let success = [];
+    let failure = [];
     let splitStr = [...str];
     let splitPattern = [...pattern];
-    splitStr = splitStr.filter(word => {
-        for (const element of splitPattern) {
-            if (element === word) {
-                return word
+
+    for (let letter = 0; letter < splitStr.length; letter++) {
+        for (let element = 0; element < splitPattern.length; element++) {
+            if (splitPattern[element] === '*' && letter === element) {
+                success.push(splitPattern[element]);
+            } else {
+                if (splitPattern[element] === splitStr[letter]) {
+                    success.push(splitPattern[element]);
+                } else if (splitPattern[element] != splitStr[letter] && letter === element) {
+                    failure.push(splitPattern[element]);
+                }
             }
         }
-    });
-    const compare = splitStr.join('');
-    if (compare.includes(pattern) && str.includes(compare)) {
-        output.push(true);
-        return output;
-    } else {
-        output.push(false);
-        return output;
     }
+    success = success.join('');
+    return { success, failure };
 }
 
 function test(str = 'Hello', pattern = 'Hello') {
-    const array = [];
-    let output = check(str, pattern, array);
+    let output;
     if (typeof str !== 'string' || typeof pattern !== 'string') {
         throw new Error(`Invalid input. Inputs must be a string`);
     } else {
-        output = output.filter(element => {
-            if (element) return element;
-        });
-        if (output[0]) {
-            return output[0]
-        } else if (!output[0]) {
-            return false
+        const { success, failure } = check(str, pattern);
+
+        if (success && success.length === str.length && success.length === pattern.length) {
+            output = `The string: ${str} matches with the pattern ${pattern}`;
+            return output;
+        } else if (failure.length !== 0) {
+            output = `The string: ${str} and the pattern ${pattern} don't match since the following characters does not match: ${failure}`;
+            return output
+        } else if (failure.length === 0) {
+            output = `The pattern's length is not big enough. Thus, the string does not match`;
+            return output;
         }
     }
 }
 
-const output = test('Hello', 'H*l*o');
+const output = test('Hex T', '*e* T');
 console.log(output);
