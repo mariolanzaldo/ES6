3d936413db3955614667a82d74c929e3c3209a46
mariolanzaldo <100377731+mariolanzaldo@users.noreply.github.com>
2022-08-15 17:17:39 -0500

    Update E30.js

--- E30.js
+++ E30.js
@@ -3,12 +3,9 @@ const list = container.querySelectorAll('p:nth-of-type(even)');
 
 container.addEventListener('click', event => {
     if (event.target.textContent.includes('Toggle') && event.target.nextElementSibling.style.display === 'flex') {
-        for (const element of list) {
-            element.style.display = 'none';
-        }
+        event.target.nextElementSibling.style.display = 'none';
     } else {
-        for (const element of list) {
-            element.style.display = 'flex';
-        }
+        event.target.nextElementSibling.style.display = 'flex';
+
     }
 });
