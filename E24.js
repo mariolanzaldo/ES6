2cff3b6cba5d7b17ecf15bfb519e2ddb9ad493b0
mariolanzaldo <100377731+mariolanzaldo@users.noreply.github.com>
2022-08-15 14:09:27 -0500

    Update E24.js

--- E24.js
+++ E24.js
@@ -1,8 +1,13 @@
 function removeElements(array, ...elements) {
-    let filteredElements = array.filter(element => {
-        return !elements.includes(element);
-    })
-    return filteredElements;
+    for (let i = 0; i < elements.length; i++) {
+        const index = array.indexOf(elements[i]);
+        if (index > -1) {
+            array.splice(index, 1);
+        } else {
+            console.error(`The element: ${elements[i]} is not present in the array`);
+        }
+    }
+    return array;
 }
 
 const output = removeElements([1, true, 5, 3, 4, 9], 5, true);
