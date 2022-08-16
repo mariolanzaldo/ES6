5f63be5b2058c57aa25c287ce53f6e58f94efbe9
mariolanzaldo <100377731+mariolanzaldo@users.noreply.github.com>
2022-08-16 14:11:37 -0500

    Update E39.js

--- E39.js
+++ E39.js
@@ -14,12 +14,18 @@ input.addEventListener('blur', event => {
 const inputValidation = (str) => {
     const input = str.toLowerCase();
     const qwerty = "1234567890qwertyuiopasdfghjklzxcvbnm";
+
     const reversedQwerty = qwerty.split('').reverse().join('');
+
     const start = 3;
     let sequence = "_" + input.slice(0, start);
     for (let i = start; i < input.length; i++) {
         sequence = sequence.slice(1) + input.charAt(i);
-        if (qwerty.includes(sequence) || reversedQwerty.includes(sequence)) {
+
+        if (qwerty.slice(0, 10).includes(sequence) || reversedQwerty.slice(0, 10).includes(sequence) ||
+            qwerty.slice(10, 20).includes(sequence) || reversedQwerty.slice(10, 20).includes(sequence) ||
+            qwerty.slice(20, 29).includes(sequence) || reversedQwerty.slice(20, 29).includes(sequence) ||
+            qwerty.slice(29, 40).includes(sequence) || reversedQwerty.slice(29, 40).includes(sequence)) {
             return false;
         }
     }
