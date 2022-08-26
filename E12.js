0b410a4db962d4c914782946383de6e3340ea595
mariolanzaldo <100377731+mariolanzaldo@users.noreply.github.com>
2022-08-26 16:38:13 -0500

    Update E12.js

--- E12.js
+++ E12.js
@@ -1,25 +1,9 @@
-function formatTime(date, format) {
-    const m = "0" + date.getMinutes();
-    const s = "0" + date.getSeconds();
-    if (format === '12h') {
-        let h = date.getHours();
-        let ap = "am";
-        if (h >= 12) {
-            ap = "pm";
-            if (h >= 13) h -= 12;
-        } else if (h == 0) {
-            h = 12;
-        }
-        h = "0" + h;
-        return `${h.slice(-2)}:${m.slice(-2)}:${s.slice(-2)} ${ap}`;
-    } else if (format === '24h') {
-        const h = '0' + date.getHours();
-        return `${h.slice(-2)}:${m.slice(-2)}:${s.slice(-2)}`;
-    } else {
-        throw new Error('Introduce a valid format');
-    }
+function YesterdayWas(date) {
+    date.setDate(date.getDate() - 1)
+    return date;
 }
 
 const date = new Date();
-const output = formatTime(date, '24h');
+console.log(date);
+const output = YesterdayWas(date);
 console.log(output);
