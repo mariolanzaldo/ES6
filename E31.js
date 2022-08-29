229475779f150ea09d39c9965f8f5c1a0bb18a64
mariolanzaldo <100377731+mariolanzaldo@users.noreply.github.com>
2022-08-29 17:53:08 -0500

    Update E31.js

--- E31.js
+++ E31.js
@@ -6,7 +6,6 @@ for (let i = 1; i < p.length; i = i + 2) {
 }
 
 container.addEventListener('click', event => {
-    console.log(event.target.nextElementSibling.getAttribute('active') === 'false')
     if (event.target.textContent.includes('Toggle') && event.target.nextElementSibling.getAttribute('active') === 'false') {
         event.target.nextElementSibling.setAttribute('active', 'true');
         event.target.nextElementSibling.style.visibility = 'visible';
@@ -14,7 +13,6 @@ container.addEventListener('click', event => {
     for (let index = 1; index < p.length; index = index + 2) {
         const element = p[index];
         if (element.getAttribute('active') === 'false') {
-            console.log(element);
             element.style.visibility = 'hidden';
         }
     }
