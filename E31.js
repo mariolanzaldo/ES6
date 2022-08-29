e21a6dfa8d4618b3c3f494eaed444bf7db65c1cb
mariolanzaldo <100377731+mariolanzaldo@users.noreply.github.com>
2022-08-29 17:52:15 -0500

    Update E31.js

--- E31.js
+++ E31.js
@@ -2,21 +2,21 @@ const container = document.querySelector('.container');
 const p = document.querySelectorAll('p');
 
 for (let i = 1; i < p.length; i = i + 2) {
-    p[i].setAttribute('active', false);
+    p[i].setAttribute('active', 'false');
 }
 
 container.addEventListener('click', event => {
-    if (event.target.textContent.includes('Toggle') && event.target.nextElementSibling.getAttribute) {
-        event.target.nextElementSibling.setAttribute('active', true);
-
-        for (let index = 1; index < p.length; index = index + 2) {
-            const element = p[index];
-            if (element.getAttribute('active') === 'true') {
-                element.style.visibility = 'visible';
-            } else if (element.getAttribute('active') === 'false') {
-                element.style.visibility = 'hidden';
-            }
-        }
-        event.target.nextElementSibling.setAttribute('active', false);
+    console.log(event.target.nextElementSibling.getAttribute('active') === 'false')
+    if (event.target.textContent.includes('Toggle') && event.target.nextElementSibling.getAttribute('active') === 'false') {
+        event.target.nextElementSibling.setAttribute('active', 'true');
+        event.target.nextElementSibling.style.visibility = 'visible';
     }
+    for (let index = 1; index < p.length; index = index + 2) {
+        const element = p[index];
+        if (element.getAttribute('active') === 'false') {
+            console.log(element);
+            element.style.visibility = 'hidden';
+        }
+    }
+    event.target.nextElementSibling.setAttribute('active', 'false');
 });
