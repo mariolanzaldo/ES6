6ccfcba8612cac5894e2124c5205788a81b1a1ab
mariolanzaldo <100377731+mariolanzaldo@users.noreply.github.com>
2022-08-29 17:28:57 -0500

    Update E30.js

--- E30.js
+++ E30.js
@@ -1,11 +1,10 @@
 const container = document.querySelector('.container');
-const list = container.querySelectorAll('p:nth-of-type(even)');
 
 container.addEventListener('click', event => {
-    if (event.target.textContent.includes('Toggle') && event.target.nextElementSibling.style.display === 'flex') {
-        event.target.nextElementSibling.style.display = 'none';
-    } else {
-        event.target.nextElementSibling.style.display = 'flex';
+    if (event.target.textContent.includes('Toggle') && event.target.nextElementSibling.style.visibility === 'visible') {
+        event.target.nextElementSibling.style.visibility = 'hidden';
+    } else if (event.target.textContent.includes('Toggle')) {
+        event.target.nextElementSibling.style.visibility = 'visible';
 
     }
 });
