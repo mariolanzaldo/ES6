a366e8ac1240d9827db196bef5815a0fbc39c935
mariolanzaldo <100377731+mariolanzaldo@users.noreply.github.com>
2022-08-29 18:54:29 -0500

    Update E39.js

--- E39.js
+++ E39.js
@@ -1,31 +1,34 @@
 const input = document.querySelector('input');
 const result = document.querySelector('.test');
 
+const qwerty = "1234567890qwertyuiopasdfghjklzxcvbnm";
+const [...qwertyLevels] = [qwerty.slice(0, 10), qwerty.slice(10, 20), qwerty.slice(20, 29), qwerty.slice(29, 40)];
+
+const reversedQwerty = qwerty.split('').reverse().join('');
+const [...reverseQwertyLevels] = [reversedQwerty.slice(0, 10), reversedQwerty.slice(10, 20), reversedQwerty.slice(20, 29), reversedQwerty.slice(29, 40)];
+
 input.addEventListener('blur', event => {
     const str = event.target.value.toLowerCase();
-    if (str.length < 4 || inputValidation(str)) {
+    const validation = inputValidation(str, qwertyLevels, reverseQwertyLevels)
+    if (str.length < 4 || validation) {
         result.textContent = 'Valid input';
-    } else if (!inputValidation(str)) {
+    } else if (!validation) {
         result.textContent = 'Invalid input';
 
     }
 });
 
-const inputValidation = (str) => {
+const inputValidation = (str, qwertyLevels, reversedQwertyLevels) => {
     const input = str.toLowerCase();
-    const qwerty = "1234567890qwertyuiopasdfghjklzxcvbnm";
-
-    const reversedQwerty = qwerty.split('').reverse().join('');
-
     const start = 3;
     let sequence = "_" + input.slice(0, start);
     for (let i = start; i < input.length; i++) {
         sequence = sequence.slice(1) + input.charAt(i);
 
-        if (qwerty.slice(0, 10).includes(sequence) || reversedQwerty.slice(0, 10).includes(sequence) ||
-            qwerty.slice(10, 20).includes(sequence) || reversedQwerty.slice(10, 20).includes(sequence) ||
-            qwerty.slice(20, 29).includes(sequence) || reversedQwerty.slice(20, 29).includes(sequence) ||
-            qwerty.slice(29, 40).includes(sequence) || reversedQwerty.slice(29, 40).includes(sequence)) {
+        if (qwertyLevels[0].includes(sequence) || reversedQwertyLevels[0].includes(sequence) ||
+            qwertyLevels[1].includes(sequence) || reversedQwertyLevels[1].includes(sequence) ||
+            qwertyLevels[2].includes(sequence) || reversedQwertyLevels[2].includes(sequence) ||
+            qwertyLevels[3].includes(sequence) || reversedQwertyLevels[3].includes(sequence)) {
             return false;
         }
     }
