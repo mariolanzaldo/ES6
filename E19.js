ac3ab2913ffd54867eba0322b19c1c0b734775f8
mariolanzaldo <100377731+mariolanzaldo@users.noreply.github.com>
2023-07-05 20:55:43 -0600

    Update E19.js

--- E19.js
+++ E19.js
@@ -10,11 +10,13 @@ function check(str, pattern) {
             match = match && (splitPattern[element] === '*' || splitPattern[element] === splitStr[letter + element]);
             if (!match) break;
 
-            if (match) success.push(splitStr[letter + element]);
+            if (match) {
+                success.push(splitStr[letter + element]);
+            }
         }
 
         if (match) {
-            return success.slice(letter, pattern.length + letter);
+            return pattern.length < str.length ? success.join('') : success.slice(letter, pattern.length + letter).join('');
         }
     }
     return null;
@@ -30,5 +32,11 @@ function test(str, pattern) {
     }
 }
 
-const output = test('Hex', '*x');
+const output = test("Hex", "x");
+// const output = test("Hex", "e*");
+// const output = test("Hex", "ex");
+// const output = test("Hex", "H*x");
+// const output = test("Hex", "**x");
+// const output = test("Hex", "****");
+
 console.log(output);
